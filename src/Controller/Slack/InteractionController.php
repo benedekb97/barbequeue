5373475c54ca44f0fbb86d8029554efb694830da
benedekb97 <benedekb97@sch.bme.hu>
2025-12-06 17:33:02 +0000

    Actually dispatch the message when an interaction comes in

--- src/Controller/Slack/InteractionController.php
+++ src/Controller/Slack/InteractionController.php
@@ -4,16 +4,19 @@ declare(strict_types=1);
 
 namespace App\Controller\Slack;
 
+use App\Message\SlackInteractionMessage;
 use App\Slack\Interaction\Component\SlackInteractionFactory;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-class InteractionController
+readonly class InteractionController
 {
     public function __construct(
         private SlackInteractionFactory $interactionFactory,
+        private MessageBusInterface $messageBus,
     ) {
     }
 
@@ -21,6 +24,8 @@ class InteractionController
     public function __invoke(Request $request): Response
     {
         $interaction = $this->interactionFactory->create($request);
+
+        $this->messageBus->dispatch(new SlackInteractionMessage($interaction));
 
         return new JsonResponse();
     }
