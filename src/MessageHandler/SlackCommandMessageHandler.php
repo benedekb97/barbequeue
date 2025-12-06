8a69fecd7babb7b1a2e223f31242c5a4b02cef2a
benedekb97 <benedekb97@sch.bme.hu>
2025-12-06 14:27:23 +0000

    Send response to given response URL

--- src/MessageHandler/SlackCommandMessageHandler.php
+++ src/MessageHandler/SlackCommandMessageHandler.php
@@ -6,8 +6,11 @@ namespace App\MessageHandler;
 
 use App\Message\SlackCommandMessage;
 use App\Slack\Command\Handler\SlackCommandHandlerInterface;
+use App\Slack\Response\Command\SlackCommandResponse;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 #[AsMessageHandler]
 readonly class SlackCommandMessageHandler
@@ -16,6 +19,8 @@ readonly class SlackCommandMessageHandler
         #[AutowireIterator(SlackCommandHandlerInterface::TAG)]
         /** @var SlackCommandHandlerInterface[] $handlers */
         private iterable $handlers,
+        private HttpClientInterface $httpClient,
+        private LoggerInterface $logger,
     ) {}
 
     public function __invoke(SlackCommandMessage $message): void
@@ -27,6 +32,17 @@ readonly class SlackCommandMessageHandler
             if ($handler->supports($command) && $command->isPending()) {
                 $handler->handle($command);
             }
+        }
+
+        if (($response = $command->getResponse()) instanceof SlackCommandResponse) {
+            $response = $this->httpClient->request('POST', $command->getResponseUrl(), [
+                'body' => json_encode($response->toArray()),
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                ],
+            ]);
+
+            $this->logger->debug($response->getContent());
         }
     }
 }
