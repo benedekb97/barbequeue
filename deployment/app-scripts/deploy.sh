932c5832c37d367aedf12cfc4e6aba2457435063
benedekb97 <benedekb97@sch.bme.hu>
2025-12-07 16:37:42 +0000

    Don't ask to run migrations

--- deployment/app-scripts/deploy.sh
+++ deployment/app-scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 git pull && \
 composer install && \
-php bin/console do:mi:mi && \
+php bin/console do:mi:mi --no-interaction && \
 sudo supervisorctl restart bbqmessenger bbqscheduler && \
 
 echo "Deployment completed successfully!"
