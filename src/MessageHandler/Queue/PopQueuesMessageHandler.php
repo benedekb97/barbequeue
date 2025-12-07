7aebbe14f5e8973dfb8f46022d758a660c0df8a3
benedekb97 <benedekb97@sch.bme.hu>
2025-12-07 16:41:22 +0000

    Dispatch removed from queue event on auto-remove

--- src/MessageHandler/Queue/PopQueuesMessageHandler.php
+++ src/MessageHandler/Queue/PopQueuesMessageHandler.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\MessageHandler\Queue;
 
+use App\Event\QueuedUser\QueuedUserRemovedEvent;
 use App\Message\Queue\PopQueuesMessage;
 use App\Repository\QueuedUserRepositoryInterface;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
@@ -15,6 +17,7 @@ readonly class PopQueuesMessageHandler
     public function __construct(
         private QueuedUserRepositoryInterface $repository,
         private EntityManagerInterface $entityManager,
+        private EventDispatcherInterface $eventDispatcher,
     ) {}
 
     public function __invoke(PopQueuesMessage $message): void
@@ -23,6 +26,10 @@ readonly class PopQueuesMessageHandler
 
         foreach ($users as $queuedUser) {
             $this->entityManager->remove($queuedUser);
+
+            $this->eventDispatcher->dispatch(
+                new QueuedUserRemovedEvent($queuedUser, $queuedUser->getQueue(), true)
+            );
         }
 
         $this->entityManager->flush();
